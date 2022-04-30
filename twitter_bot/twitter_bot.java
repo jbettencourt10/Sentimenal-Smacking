be4353a170e622a4cdbe18aad12153d7751df222
jbettencourt10 <jbettencourt10@gmail.com>
2022-04-30 08:57:52 -0400

    Git ignore files added before primary development

--- twitter_bot/twitter_bot.java
+++ twitter_bot/twitter_bot.java
@@ -5,5 +5,6 @@ public class twitter_bot {
       // This file will essentially allow us to input a query (relating to a popular event)
       // and then save the top k tweets related to that query (where k is an argument) into the data folder.
       System.out.println("Placeholder for Twitter API key will be held here.");
+      String API_KEY = "";
     }
 }
